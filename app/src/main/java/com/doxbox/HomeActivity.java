de48f249d56348510553c52bc3919e2a290c8460
Kuasy <rafaelkuasy@gmail.com>
2018-11-28 05:12:31 -0200

    gitIgnore

--- app/src/main/java/com/doxbox/HomeActivity.java
+++ app/src/main/java/com/doxbox/HomeActivity.java
@@ -47,10 +47,10 @@ public class HomeActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_movies:
-                    //mTextMessage.setText(R.string.title_movies);
+                    searchMovies();
                     return true;
                 case R.id.navigation_series:
-                    //mTextMessage.setText(R.string.title_series);
+                    searchSeries();
                     return true;
                 case R.id.navigation_twitch:
                     //mTextMessage.setText(R.string.title_twitch);
@@ -73,58 +73,64 @@ public class HomeActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+
         searchMovies();
     }
 
 
-    protected void searchMovies() {
-        SingletonRequestQueue queue = SingletonRequestQueue.getInstance(this);
-        String url = "https://ccsearch-q003.azureedge.net/indexes/0000d-movie-index/docs?api-version=2017-11-11&api-key=9454520FF92761E7FAABADB84FFBD150&search=*&$select=titleLong,summaryMedium,boxCover&$top=5";
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url,null, new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response){
-
-                        /*ImageView iv1 = (ImageView) findViewById(R.id.imageView1);
-                        ImageView iv2 = (ImageView) findViewById(R.id.imageView2);
-                        ImageView iv3 = (ImageView) findViewById(R.id.imageView3);
-                        ImageView iv4 = (ImageView) findViewById(R.id.imageView4);
-                        ImageView iv5 = (ImageView) findViewById(R.id.imageView5);
-
-
-                        ImageView[] img = {iv1, iv2, iv3, iv4, iv5};*/
-
-
-                        try {
-                            for(int i=0;i<response.getJSONArray("value").length();i++) {
-                                //img[i].setImageBitmap(); = ((JSONObject)response.getJSONArray("value").get(i)).getString("boxCover");
-
-                                String imgUrl = (((JSONObject) response.getJSONArray("value").get(i)).getJSONArray("boxCover").getString(0));
-                                imgUrl = imgUrl.replace("\\", "");
-
-                                //Picasso.get().load(imgUrl).into(img[i]);
-                                mImagesUrls.add(imgUrl);
-                                mTitles.add(((((JSONObject) response.getJSONArray("value").get(i)).getString("titleLong"))));
-                                mShortTitle.add(((((JSONObject) response.getJSONArray("value").get(i)).getString("summaryMedium"))));
-                                System.out.println("Response: " + response);
-
-                                initRecyclerView();
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }},
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        System.out.println("ERRO NA CHAMADA DO SERVIÇO DE BUSCA DE ITENS");
-                    }
-                }
-                );
-        DefaultRetryPolicy policy = new DefaultRetryPolicy(0, -1, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
-        jsonObjectRequest.setRetryPolicy(policy);
-        queue.addToRequestQueue(jsonObjectRequest);
+        protected void searchMovies() {
+            searchMedia("movie", "boxCover");
         }
+
+        protected void searchSeries() {
+            searchMedia("series", "thumbnail");
+        }
+
+        private void searchMedia(String mediaType, final String imageType){
+
+            String url = "https://ccsearch-q003.azureedge.net/indexes/0000d-"+mediaType+"-index/docs?api-version=2017-11-11&api-key=9454520FF92761E7FAABADB84FFBD150&search=*&$select=titleLong,summaryMedium,"+imageType+"&$top=5";
+            SingletonRequestQueue queue = SingletonRequestQueue.getInstance(this);
+
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url,null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response){
+
+                    try {
+                        mTitles = new ArrayList<>();
+                        mImagesUrls = new ArrayList<>();
+                        mShortTitle = new ArrayList<>();
+                        for(int i=0;i<response.getJSONArray("value").length();i++) {
+                            String imgUrl;
+                            if(imageType.equals("boxCover")) {
+                                imgUrl = (((JSONObject) response.getJSONArray("value").get(i)).getJSONArray("boxCover").getString(0));
+                            }else{
+                                imgUrl = (((JSONObject) response.getJSONArray("value").get(i)).getJSONArray("thumbnail").getString(0));
+                            }
+                            imgUrl = imgUrl.replace("\\", "");
+
+                            mImagesUrls.add(imgUrl+"&q=60&w=100");
+                            mTitles.add(((((JSONObject) response.getJSONArray("value").get(i)).getString("titleLong"))));
+                            mShortTitle.add(((((JSONObject) response.getJSONArray("value").get(i)).getString("summaryMedium"))));
+                            System.out.println("Response: " + response);
+
+                            initRecyclerView();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }},
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            System.out.println("ERRO NA CHAMADA DO SERVIÇO DE BUSCA DE ITENS");
+                        }
+                    }
+            );
+            DefaultRetryPolicy policy = new DefaultRetryPolicy(0, -1, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+            jsonObjectRequest.setRetryPolicy(policy);
+            queue.addToRequestQueue(jsonObjectRequest);
+        }
+
 
         private void initRecyclerView() {
             Log.d(TAG, "initRecyclerView: init recycleview.");
@@ -133,4 +139,6 @@ public class HomeActivity extends AppCompatActivity {
             recyclerView.setAdapter(adapter);
             recyclerView.setLayoutManager(new LinearLayoutManager(this));
         }
+
+
     }
